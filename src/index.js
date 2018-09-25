94a2eee4e0221f32b70c5d104006d47ad41eb04e
ryanschmid <schmid.ryan.c@gmail.com>
2018-09-25 18:04:04 -0500

    Finished GET, POST refactor; DELETE pending & PUT

--- src/index.js
+++ src/index.js
@@ -1,156 +1,193 @@
 "use strict";
 
-
 // ##### INITIAL GET REQUEST & LOADING MOVIES ##### //
-function getMovies() {
-    $.get('/api/movies')
-        .then( $('#welcome').text("Loading..."))
-        .done(function (movies, status, jqXhr) {
-            console.log(movies);
-            $('#welcome').text("Movie Time!")
-            $('#insertMovies').html(renderMovies(movies));
-        })
-        .fail(function (jqXHR, textStatus, errorThrown) {
-            console.log(textStatus, errorThrown);
-        });
-    console.log('GET complete');
-}
-getMovies();
+// function getMovies() {
+//     $.get('/api/movies')
+//         .then( $('#welcome').text("Loading..."))
+//         .done(function (movies, status, jqXhr) {
+//             console.log(movies);
+//             $('#welcome').text("Movie Time!")
+//             $('#insertMovies').html(renderMovies(movies));
+//         })
+//         .fail(function (jqXHR, textStatus, errorThrown) {
+//             console.log(textStatus, errorThrown);
+//         });
+//     console.log('GET complete');
+// }
+// getMovies();
 
 // ##### RENDER MOVIES ##### //
 function renderMovies(movies) {
     let newHtml = '';
     movies.forEach(function (movie) {
-        newHtml += `<tr><td>${movie.title}</td><td>${movie.rating}</td><td><button id ="edit${movie.id}">Edit</button><button id ="delete${movie.id}">Delete</button></td>`
-        });
-    return newHtml;
+        newHtml += `<tr><td>${movie.title}</td>
+                    <td>${movie.rating}</td>
+                    <td><button class="edit" data-id="${movie.id}">Edit</button>
+                    <button class="delete" data-id ="${movie.id}">Delete</button></td></tr>`
+    });
+    // return newHtml;
+    return (newHtml);
 }
 
 
+function getMovies() {
+    const url = '/api/movies/'
+    let contentPromise = fetch(url);
+    contentPromise.catch(error => {
+        console.log(error)
+    });
+    contentPromise.then(response => {
+        response.json().then(data => {
+            console.log(data);
+            $('#insertMovies').html(renderMovies(data));
+        })
+            .catch(error => {
+                console.log(error)
+            })
+    });
+}
+
+getMovies();
+
+
 // ###### DELETE MOVIES ##### //
-const deleteMovie = {
-    function() {
-        event.preventDefault();
-        $.ajax({
-            url: '/api/movies',
-            type: "DELETE"
-        })
-            .then($('#welcome').text("Loading..."))
-            .done(function (data, status, jqXhr) {
-                console.log(data);
-                $('#welcome').text("Movie Time!")
-                $('#insertMovies').html(renderMovies(data));
-            })
-            .fail(function (jqXHR, textStatus, errorThrown) {
-                console.log(textStatus, errorThrown);
-            });
+// const deleteMovie = {
+//     function() {
+//         event.preventDefault();
+//         $.ajax({
+//             url: '/api/movies',
+//             type: "DELETE"
+//         })
+//             .then($('#welcome').text("Loading..."))
+//             .done(function (data, status, jqXhr) {
+//                 console.log(data);
+//                 $('#welcome').text("Movie Time!")
+//                 $('#insertMovies').html(renderMovies(data));
+//             })
+//             .fail(function (jqXHR, textStatus, errorThrown) {
+//                 console.log(textStatus, errorThrown);
+//             });
+//
+//     }
+// };
 
-    }
+
+// const editMovies = (data, id) => {
+//     return fetch(`/api/movies/${id}`, {
+//         method: "PUT",
+//         body: JSON.stringify(data),
+//         headers: {'Content-Type': 'application/json'},
+//     }).then(
+//         response => console.log('Success: ' + JSON.stringify(response)))
+//         .catch(error => console.log('Fail: ' + JSON.stringify(error)))
+// };
+
+
+//###### ADD MOVIES ##### //
+
+
+function addMovie() {
+    let data = {};
+    data.title = $("#inputTitle").val();
+    data.rating = $("#inputRating").val();
+    let addContentPromise = fetch('/api/movies/', {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {'Content-Type': 'application/json'}
+    });
+    addContentPromise.catch(error => {
+        console.log(error)
+    });
+    addContentPromise.then(response => getMovies());
 };
 
 
-// ###### ADD MOVIES ##### //
-const addMovie = {
-    function() {
-        event.preventDefault();
-        $.ajax({
-            url: '/api/movies',
-            type: "POST",
-            data: {
-                "title": $("#inputTitle").val(),
-                "rating": $("#inputRating").val()
-            }
-        })
-            .then($('#welcome').text("Loading..."))
-            .done(function (data, status, jqXhr) {
-                console.log(data);
-                $('#welcome').text("Movie Time!")
-                $('#insertMovies').html(renderMovies(data));
-            })
-            .fail(function (jqXHR, textStatus, errorThrown) {
-                console.log(textStatus, errorThrown);
-            });
+let deleteButtons = document.querySelectorAll(".delete");
+deleteButtons = Array.from(deleteButtons);
 
-    }
-};
+deleteButtons.forEach(button => {
+    button.addEventListener('click', function (event) {
+        event.preventDefault();
+        let id = event.target.getAttribute("data-id");
+        //with edit
+
+        let url = "api/movies/" + id;
+        fetch(url, {
+            method: "DELETE",
+        }).then(response => getMovies())
+    })
+});
+
+// let editButtons = document.querySelectorAll(".edit");
+// editButtons = Array.from(editButtons);
+//
+// editButtons.forEach(button => {
+//     button.addEventListener('click', function(event) {
+//         event.preventDefault();
+//         let id = event.target.getAttribute("data-id");
+//         //with edit
+//
+//         let url = "api/movies/" + id;
+//         let data = {};
+//         // data.title =
+//         // data.rating =
+//
+//         fetch(url, {
+//             method:POST,
+//             body: JSON.stringify(data),
+//             headers: {'Content-Type': 'application/json'}
+//         })
+//
+//     })
+//     }
+
+
 
 // ###### EDIT MOVIES ##### //
-const editMovie = {
-    function() {
-        event.preventDefault();
-        $.ajax({
-            url: '/api/movies',
-            type: "PUT",
-            data: {
-                "title": $("#inputTitle").val(),
-                "rating": $("#inputRating").val()
-            }
-        })
-            .then($('#welcome').text("Loading..."))
-            .done(function (data, status, jqXhr) {
-                console.log(data);
-                $('#welcome').text("Movie Time!")
-                $('#insertMovies').html(renderMovies(data));
-            })
-            .fail(function (jqXHR, textStatus, errorThrown) {
-                console.log(textStatus, errorThrown);
-            });
+// const editMovie = {
+//     function() {
+//         event.preventDefault();
+//         $.ajax({
+//             url: '/api/movies',
+//             type: "PUT",
+//             data: {
+//                 "title": $("#inputTitle").val(),
+//                 "rating": $("#inputRating").val()
+//             }
+//         })
+//             .then($('#welcome').text("Loading..."))
+//             .done(function (data, status, jqXhr) {
+//                 console.log(data);
+//                 $('#welcome').text("Movie Time!")
+//                 $('#insertMovies').html(renderMovies(data));
+//             })
+//             .fail(function (jqXHR, textStatus, errorThrown) {
+//                 console.log(textStatus, errorThrown);
+//             });
+//
+//     }
+// };
 
-    }
-};
 
-
-
+document.getElementById('addMovie').addEventListener("click", function (event) {
+    event.preventDefault();
+    addMovie();
+});
+// document.getElementById('deleteMovie').addEventListener("click", deleteMovie);
 
 
 // const form = document.getElementById('addMovieForm');
 // form.addEventListener('submit', addMovie);
 
-// const url = '/api/movies'
-// let inputTitle = $("#inputTitle").val()
-// let inputRating = $("#inputRating").val()
+
 // let inputId = 6
 // let addedMovie = {'title': `${inputTitle}`, 'rating': `${inputRating}`, 'id': `${inputId}`};
-
-
-
-document.getElementById('addMovie').addEventListener("click", addMovie);
-document.getElementById('deleteMovie').addEventListener("click", deleteMovie);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // fetch(url, )
 //     .then(movies => {return movies.json()})
 //     .then(res => {console.log(res)}
 //     .catch(error => {console.log(error)}
-
 
 
 //     $.ajax({
@@ -167,8 +204,6 @@ document.getElementById('deleteMovie').addEventListener("click", deleteMovie);
 
 // // ###### DOM EVENT TRIGGERS ##### //
 // document.getElementById('deleteMovie').addEventListener("click", deleteMovies());
-
-
 
 // Allow users to edit existing movies: Give users the option to edit an existing movie
 // A form should be pre-populated with the selected movie's details
